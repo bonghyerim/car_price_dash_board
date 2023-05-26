dd7bb8a371c5f537e9e3d848d338a61c49f548f6
bonghyerim <130967304+bonghyerim@users.noreply.github.com>
2023-05-26 12:32:07 +0900

    1.2

--- app_home.py
+++ app_home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def run_app_home():
     
-    st.subheader('이재은 바보')
+    st.subheader('안녕하세요!')
     st.markdown('좋은 서비스로 제공하겠습니다.')
 
     st.markdown('이 앱은 고객 데이터의 자동차 구매 데이터에 대한 내용입니다.')
