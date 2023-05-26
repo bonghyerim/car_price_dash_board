e434daa577b63f5dc21082d836ce61fec95f2e17
bonghyerim <130967304+bonghyerim@users.noreply.github.com>
2023-05-26 14:36:41 +0900

    3.3

--- app_home.py
+++ app_home.py
@@ -4,6 +4,7 @@ def run_app_home():
     
     st.subheader('안녕하세요!')
     st.markdown('좋은 서비스로 제공하겠습니다.')
+    st.markdown('자동 배포 처리된 앱입니다.')
 
     st.markdown('이 앱은 고객 데이터의 자동차 구매 데이터에 대한 내용입니다.')
     st.markdown('데이터 분석도 가능하고, 고객 정보를 넣으면 구매 차량 가격도 예측해 줍니다.')
